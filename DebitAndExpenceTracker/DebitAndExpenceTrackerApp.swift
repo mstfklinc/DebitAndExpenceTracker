a380b6731b540b3c5e53911147652c674ccac972
Mustafa KILINÇ <mustafakilinc429@gmail.com>
2023-12-29 20:52:17 +0300

    Initial Commit

---
+++ DebitAndExpenceTracker/DebitAndExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebitAndExpenceTrackerApp.swift
+//  DebitAndExpenceTracker
+//
+//  Created by Mustafa Kılınç on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DebitAndExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
